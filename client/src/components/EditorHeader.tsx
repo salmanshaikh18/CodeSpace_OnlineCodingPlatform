80fced4ca1eb4eac26fa1df700d9716fa7aabdff
Salman Shaikh <salmanshaikh18786@gmail.com>
2024-06-27 10:06:58 +0530

    save code frontend

--- client/src/components/EditorHeader.tsx
+++ client/src/components/EditorHeader.tsx
@@ -15,16 +15,38 @@ import {
   updateCurrentLanguage,
 } from "@/app/features/codeEditorSlice";
 import { RootState } from "@/app/store";
+import { handleError } from "@/utils/handleErrors";
+import axios from "axios";
 
 const EditorHeader = () => {
   const dispatch = useDispatch();
   const currentLanguage = useSelector(
     (state: RootState) => state.codeEditorSlice.currentLanguage
   );
+  const fullCode = useSelector(
+    (state: RootState) => state.codeEditorSlice.fullCode
+  );
+  const handleSaveCode = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/code-editor/save",
+        {
+          fullCode: fullCode,
+        }
+      );
+      console.log(`Resonse: ${response}`);
+      console.log(`Response.data: ${response.data}`);
+    } catch (error) {
+      handleError(error);
+    }
+  };
   return (
     <div className="sm:h-[50px] h-[100px] flex-wrap sm:flex-nowrap bg-zinc-900 text-white flex justify-between px-4 items-center">
       <div className="flex gap-4">
-        <button className="px-4 py-1 flex justify-center items-center gap-2 bg-green-600 rounded-md hover:bg-green-700 transition-all ease-in-out duration-300 hover:scale-105">
+        <button
+          onClick={handleSaveCode}
+          className="px-4 py-1 flex justify-center items-center gap-2 bg-green-600 rounded-md hover:bg-green-700 transition-all ease-in-out duration-300 hover:scale-105"
+        >
           <FaCloud />
           Save
         </button>
